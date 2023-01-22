f25bb15e13f0a86e700488d8993cb0b9febbd587
Regina <gumerovars@gmail.com>
2023-01-22 16:43:59 +0500

    second task

---
+++ ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿int searchSum(int m, int n)
+{
+    if (n == m) return m;
+    return n + searchSum(m, n - 1);
+}
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+searchSum(m, n);
+Console.WriteLine("Сумма натуральных чисел в заданом промежутке = " + searchSum(m, n));

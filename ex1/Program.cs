4744bc216300540864ae5e6c6662df31afd877ad
Regina <gumerovars@gmail.com>
2023-01-22 16:43:45 +0500

    first task

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿void printNaturalNumbers(int n)
+{
+    if (n == 0) return;
+    Console.Write(n + ",");
+    printNaturalNumbers(n-1);
+}
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+printNaturalNumbers(n);
+Console.WriteLine();
